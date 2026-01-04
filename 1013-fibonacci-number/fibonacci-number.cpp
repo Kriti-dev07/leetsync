f1dec16ce935666f6272228533054c9af49d4047
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-01-05 02:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.08%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first_number = 0, second_number = 1; //starts at 0,1...
+        for (int i = 2; i <= n; i++) {  //looping starts at i=2 and moves like i=34,5.. so on till it reaches n 
+            int c = first_number + second_number; // fir first iteration c=1(0+1) then or second a=1 and b=1 so c=2 simlilarly for third a=1b=2 so c=3
+            // and for 4 th a=2 and b=3 so c becomes 5
+            first_number = second_number;
+            second_number = c;
+        }
+        return second_number;
+    }
+};
