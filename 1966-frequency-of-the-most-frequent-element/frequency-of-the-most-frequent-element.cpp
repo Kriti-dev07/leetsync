14434f3c693c339d52fd69a947a866622cd3705a
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-01-12 00:20:39 +0530

    Time: 35 ms (76.72%) | Memory: 105.8 MB (90.49%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -3,19 +3,19 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
 
-        long long sum = 0;  //sum of current window
-        int l = 0;     //left pointer of window 
-        int ans = 0;     //maximum window size answer 
+        long long sum = 0;
+        int l = 0;
+        int ans = 0;
 
-        for (int r = 0; r < nums.size(); r++) {   //we keep expanding window to the right 
+        for (int r = 0; r < nums.size(); r++) {
             sum += nums[r];
 
-            while ((long long)nums[r] * (r - l + 1) - sum > k) {   
+            while ((long long)nums[r] * (r - l + 1) - sum > k) {
                 sum -= nums[l];
                 l++;
             }
 
-            ans = max(ans, r - l + 1);   // needed to find best answer ..give example[1,2,4,8] answer will be 2 in this case but better answer 3 is vailable 
+            ans = max(ans, r - l + 1);
         }
         return ans;
     }
