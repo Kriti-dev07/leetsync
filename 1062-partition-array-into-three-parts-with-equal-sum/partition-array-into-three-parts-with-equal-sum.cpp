db788c3f2ae40604f42fbfd3240766ed918aa407
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-01-26 15:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 36.4 MB (11.72%) - LeetSync

--- 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& arr) {
-        int total = 0;
-        for (int x : arr) total += x;
-
-        if (total % 3 != 0) return false;
-
-        int target = total / 3;
-        int currSum = 0;
-        int count = 0;
-
-        for (int i = 0; i < arr.size(); i++) {
-            currSum += arr[i];
-            if (currSum == target) {
-                count++;
-                currSum = 0;
-            }
+        int total=0;
+        for(int x: arr){
+            total+=x;
         }
-
-        return count >= 3;
+        if(total%3!=0)return false;
+        int target=total/3;
+        int current_sum=0;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            current_sum+=arr[i];
+            if(current_sum==target){
+                count++;
+                current_sum=0;
+            }}
+            if(count>=3){
+                return true;
+            }
+        else {
+            return false;}
     }
 };
