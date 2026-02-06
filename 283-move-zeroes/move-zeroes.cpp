166d904a1b4096ab844a4a326f155d616ab74e67
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-02-06 14:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,14 +5,15 @@ public:
 
         // move non-zero elements forward
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
+            if (nums[i] != 0) {                 // here we overwrite values but doesnt matter once loops reaches end of array we are done scanning and in the final array space left is filled by zeros
                 nums[idx++] = nums[i];
             }
         }
 
         // fill remaining positions with zero
         while (idx < nums.size()) {
-            nums[idx++] = 0;
+            nums[idx++] = 0; // alternate way to write nums[idx]; idx++;
+
         }
     }
 };
