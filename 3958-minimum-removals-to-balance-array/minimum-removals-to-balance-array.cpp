188ea1b031eb93c7a698a1db8cf4f9ab296152f0
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-02-06 18:26:31 +0530

    Time: 38 ms (35.46%) | Memory: 105.1 MB (52.91%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0;
+        int maxLen = 1;
+
+        for (int right = 0; right < n; right++) {
+            while ((long long)nums[left] * k < nums[right]) {
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return n - maxLen;
+    }
+};
