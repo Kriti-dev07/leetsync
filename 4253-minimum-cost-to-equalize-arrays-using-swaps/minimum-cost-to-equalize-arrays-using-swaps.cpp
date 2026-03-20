6fbe79aa9b11067135889361310f029ec823f8bf
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-03-20 14:20:43 +0530

    Time: 341 ms (49.30%) | Memory: 315.3 MB (33.82%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -1,22 +1,31 @@
- class Solution {
+class Solution {
 public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
-     vector<int> t=nums1;
-        unordered_map<int,int> freq;
-        int n=nums1.size();
-        for(int x:nums1)freq[x]++;
-        for(int x:nums2)freq[x]++;
+        unordered_map<int, int> cnt1, cnt2;
 
-        for(auto&p :freq){
-            if(p.second % 2) return -1;
+        for (int x : nums1) cnt1[x]++;
+        for (int x : nums2) cnt2[x]++;
+
+        unordered_set<int> allVals;
+        for (auto& [v, _] : cnt1) allVals.insert(v);
+        for (auto& [v, _] : cnt2) allVals.insert(v);
+
+        int totalExcess = 0;
+
+        for (int v : allVals) {
+            int c1 = cnt1.count(v) ? cnt1[v] : 0;
+            int c2 = cnt2.count(v) ? cnt2[v] : 0;
+            int total = c1 + c2;
+
+            if (total % 2 != 0) return -1;
+
+            int needed = total / 2;
+
+            if (c1 > needed) {
+                totalExcess += (c1 - needed);
+            }
         }
-        unordered_map<int, int> diff;
-        for(int x:nums1) diff[x]++;
-        for(int x:nums2) diff[x]--;
-        long long ans=0;
-        for(auto &p :diff){
-            ans+=abs(p.second);
-        }
-        return ans/4;
+
+        return totalExcess;
     }
 };
