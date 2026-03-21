a7bd008172aca58991167b7a38d2f58b5edf38f3
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-03-21 10:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (19.54%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -5,16 +5,17 @@ public:
         if(!head || !head->next)
             return head;
 
-        ListNode* dummyNode = new ListNode(0, head);
+        ListNode* dummyNode = new ListNode(0, head); //creates a node named dummy and its value is 0 while its next is head 
 
-        ListNode* prevNode = dummyNode;
+        ListNode* prevNode = dummyNode;        
         ListNode* currNode = head;
 
-        while(currNode && currNode->next){
+        while(currNode && currNode->next){  //currNode and currNode->next node exists 
 
             prevNode->next = currNode->next;
+          
             currNode->next = currNode->next->next;
-            prevNode->next->next = currNode;
+              prevNode->next->next = currNode;
 
             prevNode = currNode;
             currNode = currNode->next;
