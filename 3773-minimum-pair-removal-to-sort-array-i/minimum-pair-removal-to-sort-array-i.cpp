f29d5cc5b18140bb7df506bf2ab70845d669b9f8
KRITI MITTAL <erdeepti.ag@gmail.com>
2026-04-03 18:16:52 +0530

    Time: 3 ms (29.47%) | Memory: 27.8 MB (19.31%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,34 +1,70 @@
 class Solution {
 public:
-    bool isSorted(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < nums[i - 1]) return false;
-        }
-        return true;
-    }
-
     int minimumPairRemoval(vector<int>& nums) {
+        int n = nums.size();
+
+        // Doubly linked list
+        vector<int> left(n), right(n);
+        for (int i = 0; i < n; i++) {
+            left[i] = i - 1;
+            right[i] = (i + 1 < n ? i + 1 : -1);
+        }
+
+        // Min heap: {sum, index}
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+        for (int i = 0; i < n - 1; i++) {
+            pq.push({nums[i] + nums[i + 1], i});
+        }
+
+        vector<bool> alive(n, true);
         int ops = 0;
 
-        while (!isSorted(nums)) {
-            int n = nums.size();
-            int minSum = INT_MAX;
-            int idx = 0;
+        // Check if sorted
+        auto isSorted = [&]() {
+            int prev = -1;
+            for (int i = 0; i != -1; i = right[i]) {
+                if (!alive[i]) continue;
 
-            // find leftmost pair with minimum sum
-            for (int i = 0; i < n - 1; i++) {
-                int sum = nums[i] + nums[i + 1];
-                if (sum < minSum) {
-                    minSum = sum;
-                    idx = i;
-                }
+                if (prev != -1 && nums[i] < nums[prev])
+                    return false;
+
+                prev = i;
+            }
+            return true;
+        };
+
+        while (!isSorted()) {
+            auto [sum, i] = pq.top();
+            pq.pop();
+
+            int j = right[i];
+
+            // Skip invalid structure
+            if (j == -1 || !alive[i] || !alive[j]) continue;
+
+            // ❗ CRITICAL FIX: skip stale heap entries
+            if (nums[i] + nums[j] != sum) continue;
+
+            // Merge
+            nums[i] += nums[j];
+            alive[j] = false;
+
+            // Fix links
+            right[i] = right[j];
+            if (right[j] != -1) {
+                left[right[j]] = i;
             }
 
-            // merge the pair
-            nums[idx] += nums[idx + 1];
-            nums.erase(nums.begin() + idx + 1);
-
             ops++;
+
+            // Push updated adjacent pairs
+            if (left[i] != -1 && alive[left[i]]) {
+                pq.push({nums[left[i]] + nums[i], left[i]});
+            }
+            if (right[i] != -1 && alive[right[i]]) {
+                pq.push({nums[i] + nums[right[i]], i});
+            }
         }
 
         return ops;
